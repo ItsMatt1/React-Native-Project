0ca0b18b84b55ed79e8f2966934c8f0faefc2141
Matheus de Moura <dev02@neokoros.com>
2022-09-29 15:48:50 -0300

    new changes

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 // import React from 'react'; NOT NEEDED
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
+import Cesta from './src/screens/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>TESTE</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar/>
+      <Cesta/>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
